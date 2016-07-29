35da2ea3b539027a732303251e7233619310bbb7
SunShine <sunshine@bogon>
2016-07-29 17:15:00 +0800

    Initial Commit

---
+++ ListDropProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ListDropProject
+//
+//  Created by SunShine on 16/7/29.
+//  Copyright © 2016年 SunShuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ListDropProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ListDropProject
+//
+//  Created by SunShine on 16/7/29.
+//  Copyright © 2016年 SunShuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
